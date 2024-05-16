47f1e4925ffbff6c053d0e8061a153e53b1dbd53
makzuu <mr.greenmousth@gmail.com>
2024-05-15 20:04:29 -0400

    Draw some lines

--- window.py
+++ window.py
@@ -1,4 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
+from point import Point
+from line import Line
 
 class Window:
     def __init__(self, width, height):
@@ -21,6 +23,12 @@ class Window:
     def close(self):
         self.running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
 if __name__ == "__main__":
     window = Window(800, 600)
+    window.draw_line(Line(Point(0, 300), Point(800, 300)), "#ccc")
+    window.draw_line(Line(Point(100, 80), Point(700, 520)), "#ccc")
+    window.draw_line(Line(Point(100, 200), Point(700, 400)), "#ccc")
     window.wait_for_close()
